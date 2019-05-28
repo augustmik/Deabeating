b200834c9e480c6803b3b724b46f76e2c4a19824
Ranttis <roope.rantasalo@gmail.com>
2019-05-28 17:58:30 +0200

    dsaads
    adsads

--- Assets/scripts/Player Scripts/Click_handler.cs
+++ Assets/scripts/Player Scripts/Click_handler.cs
@@ -1,46 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Ink.Runtime;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-
 
 public class Click_handler : MonoBehaviour
 {
     public GameObject Clickable;
-    private GameObject callable;
     //public GameObject dialogue;
-    private HealthBar hpBar;
+
     private Vector3 pos = new Vector3(0, 0, 0);
     private Quaternion quat;
 
     public string levelName;
+    public Text NoAccess;
 
-    private void Awake()
-    {
+    
 
-    }
+   
+
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (levelName == "HandleMethod")
+            if(levelName != SceneManager.GetActiveScene().name)
             {
-                callable = GameObject.Find("HealthBar");
-                hpBar = callable.GetComponent<HealthBar>();
-                hpBar.SetSizeL();
-
-            }
-            else if (levelName != SceneManager.GetActiveScene().name)
-            {
-                SceneManager.LoadScene(levelName);
+                if(levelName == "Home" && GameManager.Instance.marketEventCompleted == false)
+                {
+                    NoAccess.text = "You have to go to the market first";
+                }
+             SceneManager.LoadScene(levelName);
+              
             }
             
         }
     }
 
 
-
+    
 
 }
