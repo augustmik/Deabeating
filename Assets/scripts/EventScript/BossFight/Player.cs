7faf6ef7d35166fac0fa1543ab640337816d950f
Roope <roope.rantasalo@gmail.com>
2019-06-04 15:54:26 +0200

    asd

---
+++ Assets/scripts/EventScript/BossFight/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Image playerHPBar;
+    public float reduceHPAmount;
+    
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if player is hit
+    }
+
+    public void reducePlayerHP()
+    {
+        playerHPBar.fillAmount -= reduceHPAmount;
+    }
+}
