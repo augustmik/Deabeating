1bdaf9fe483114a6a2f9b7644bb91e631438f0d9
Roope <roope.rantasalo@gmail.com>
2019-06-04 13:02:29 +0200

    bossu

---
+++ Assets/scripts/EventScript/BossFight/DodgeUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeUp : MonoBehaviour
+{
+    public bool playerMove = false;
+    public GameObject playerObject;
+    public float dodgeAmount;
+    public float waitTime;
+    Dodge otherScript;
+    private float timer = 0.0f;
+    void Start()
+    {
+        GameObject otherGO = GameObject.Find("DodgeArrow");
+        otherScript = otherGO.GetComponent<Dodge>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > waitTime && playerMove == true)
+        {
+            playerObject.transform.Translate(0, dodgeAmount * -1, 0);
+            timer = timer - waitTime;
+            playerMove = false;
+        }
+
+        if (timer > waitTime * 2) { timer -= waitTime * 2; }
+    }
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && playerMove == false && otherScript.playerMove == false)
+        {
+            playerMove = true;
+            playerObject.transform.Translate(0, dodgeAmount, 0);
+            timer = 0.0f;
+        }
+    }
+}
---
+++ Assets/scripts/EventScript/BossFight/Throwable.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    private SpriteRenderer sRend;
+    public Sprite colaSprite;
+    public Sprite chipsSprite;
+    private Vector3 startPos;
+    private int randomA;
+    private float timer = 0.0f;
+    private float waitTime = 3.0f;
+    public int cooldown;
+    private bool colaCheck = false;
+    bool failsafe = true;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        sRend = GetComponent<SpriteRenderer>();
+        startPos = new Vector3(transform.position.x, transform.position.y);
+        randomA = Random.Range(0, 2);
+        switch (randomA)
+        {
+            case 0:
+                ThrowChips();
+                break;
+            case 1:
+                ThrowCola();
+                break;
+            default:
+                Debug.Log("Switch broke");
+                break;
+        }
+
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (colaCheck && timer >= 0.2f)
+        {
+            rBody.velocity = new Vector2(0, 0);
+            transform.Translate(new Vector3(750, 415, 0));    //Cola attack move
+            colaCheck = false;
+        }
+
+        if (transform.position.y <= -400.0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    public void ThrowChips()
+    {
+        sRend.sprite = chipsSprite;
+        rBody.velocity = new Vector2(600, -50);
+    }
+    public void ThrowCola()
+    {
+        colaCheck = true;
+        sRend.sprite = colaSprite;
+        rBody.velocity = new Vector2(170, 700);
+    }
+
+}
