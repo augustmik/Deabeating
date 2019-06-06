ec87eb1d07859742da5704cc5f2bd1cc79df44fb
Roope <roope.rantasalo@gmail.com>
2019-06-06 12:07:42 +0200

    asd

--- Assets/scripts/EventScript/School/FactHandler.cs
+++ Assets/scripts/EventScript/School/FactHandler.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FactHandler : MonoBehaviour
 {
     public static FactsRoot allFacts;
-
+    private static int currentFact = 0;
+    public Text teacherText;
     void Start()
     {
         var jsonTextFile = Resources.Load<TextAsset>("Facts");
         allFacts = JsonUtility.FromJson<FactsRoot>(jsonTextFile.ToString());
-        //goalText.text = allFacts.Fact[1].factText;
+        //goalText.text = allFacts.Facts[1].factText;
 
     }
 
@@ -21,5 +24,21 @@ public class FactHandler : MonoBehaviour
     public static FactsRoot CreateFromJSON(string jsonString)
     {
         return JsonUtility.FromJson<FactsRoot>(jsonString);
+    }
+    public void LoadNextFact()
+    {
+        try
+        {
+            teacherText.text = allFacts.Facts[currentFact].factText;
+            //do something with facts if (currentFact==n) show something?
+            currentFact++;
+        }
+        catch (System.ArgumentOutOfRangeException e) { EndSchoolEvent(); }
+    }
+    public void EndSchoolEvent()
+    {
+        teacherText.text = "That would be all kids. You are free to go.";
+        Debug.Log("Event End");
+        //Hook this to some scene
     }
 }
